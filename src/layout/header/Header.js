11c378591db42d642fa07218b3bc359ab4e40c3c
Dima <dimaprog1991@gmail.com>
2021-12-23 17:26:38 +0200

    [WIP] changed background and colors

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -5,17 +5,19 @@ import './Header.scss';
 
 export default function Header (props) {
     return (
-        <div className="header-wrapp">
+        <div className="header-wrap">
             <nav className="navbar fixed-top">
                 <div className="container">
                     <div className="row w-100">
-                        <div className="col-2 toogle-button-wrap">
-                            <button type="button" onClick={ ()=> props.onToggle() } className="btn btn-info">
-                                <i className="bi bi-person-lines-fill"></i>
-                            </button>
-                        </div>
-                        <div className="col-10">
-                            <h1 className="main-title text-center">My Resume</h1>
+                        <div className="col-10 header">
+                            <div className="toogle-button-wrap">
+                                <button type="button" onClick={ ()=> props.onToggle() } className="btn">
+                                    <i className="bi bi-person-lines-fill"></i>
+                                </button>
+                            </div>
+                            <div className="header-text-wrap">
+                                <h1 className="main-title text-center">My Resume</h1>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/layout/side/Side.js
+++ src/layout/side/Side.js
@@ -1,21 +1,36 @@
 import './Side.scss';
-import personal from '../../data/personal_information.json';
+import person from '../../data/personal_information.json';
 import boy from '../../images/superboy.jpg';
 
 export default function Side (props) {
     return (
-        <div className={"col left-side-wrapp " + (props.toggle ? "show" : "")}>
+        <div className={"col left-side-wrap " + (props.toggle ? "show" : "")}>
             <div className="side-left">
                 <div className="image-with-name text-center">
                     <img className="boy" src={boy} alt="I am"/>
-                    <h3>{personal.name}</h3>
-                    <h4>{personal.position}</h4>
+                    <h3>{person.personal_info.name}</h3>
+                    <h4>{person.personal_info.position}</h4>
                 </div>
-                <div className="personal-wrap">
+                <div className="item-wrap">
                     <h4 className="title">Personal Info</h4>
                     <div className="item">
                         <div className="item-name">E-mail</div>
-                        <div className="item-description">{personal.email}</div>
+                        <div className="item-description">{person.personal_info.email}</div>
+                    </div>
+                </div>
+                <div className="item-wrap">
+                    <h4 className="title">Education Info</h4>
+                    <div className="item">
+                        <div className="item-name">Dates of studying</div>
+                        <div className="item-description">{person.education.dates_of_studying}</div>
+                    </div>
+                    <div className="item">
+                        <div className="item-name">Name of institution</div>
+                        <div className="item-description">{person.education.name}</div>
+                    </div>
+                    <div className="item">
+                        <div className="item-name">Degree and major</div>
+                        <div className="item-description">{person.education.degree}</div>
                     </div>
                 </div>
             </div>
