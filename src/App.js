357ff00fc5614ccd5ba93cb4bc8a3b20506099df
Dima <dimaprog1991@gmail.com>
2021-12-23 19:01:39 +0200

    [WIP] added new data and changed content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import Header from './layout/header/Header';
 import Side from './layout/side/Side';
+import experience_data from './data/experience.json'
 
 function App() {
 
@@ -19,8 +20,22 @@ function App() {
             <Side toggle={toggle} />
             <div className="container">
                 <div className="row d-flex justify-content-center w-100">
-                    <div className="col col-sm-12 col-md-10 col-lg-10 content-wrap">
-                        content
+                    <div className="col col-sm-12 col-md-10 col-lg-8 content-wrap">
+                        <h3 className='title'>Work Experience</h3>
+                        <div className="experience-wrap">
+                            {
+                                experience_data.map((item) => {
+                                    console.log(item)
+                                   return(
+                                       <div className='item'>
+                                           <div className='name'>{item.name_employer}</div>
+                                           <div className='position'>{item.position}</div>
+                                           <div className='date-employment'>{item.dates_employment}</div>
+                                       </div>
+                                   );
+                                })
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
