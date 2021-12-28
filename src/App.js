d7fe9faa39ecfe976cefbfef0f024c10de81a0b6
Dima <dimaprog1991@gmail.com>
2021-12-28 13:25:03 +0200

    [WIP] changed info and style

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
             <div className="container">
                 <div className="row d-flex justify-content-center w-100 mx-0">
                     <div className="col col-sm-12 col-md-10 col-lg-8 content-wrap">
-                        <h3 className='title'>Work Experience</h3>
+                        <h3 className='main-title'>Work Experience</h3>
                         <div className="experience-wrap">
                             {
                                 experience_data.map((item) => {
@@ -31,6 +31,20 @@ function App() {
                                            <div className='name'>{item.name_employer}</div>
                                            <div className='position'>{item.position}</div>
                                            <div className='date-employment'>{item.dates_employment}</div>
+                                           <div className='title'>Projects:</div>
+                                           <div className='projects-wrap'>
+                                               {
+                                                   item.projects.map((project) => {
+                                                     return (
+                                                         <div className='project'>
+                                                             <div className='name'>{project.name}</div>
+                                                             <div className='role'>{project.role}</div>
+                                                             <div className='technologies'>{project.technologies}</div>
+                                                         </div>
+                                                     )
+                                                   })
+                                               }
+                                           </div>
                                        </div>
                                    );
                                 })
